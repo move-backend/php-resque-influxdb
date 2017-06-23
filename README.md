713968ceb2a6bd642a4af308e963307fd177ae05
Sean Molenaar <sean@m2mobi.com>
2017-06-23 14:04:50 +0200

    Core: PSR-4 loading and influxDB

--- README.md
+++ README.md
@@ -1,121 +1,75 @@
-php-resque-statsd: PHP Resque StatsD
+php-resque-influxdb: PHP Resque InfluxDB
 ==========================================
 
-php-resque-statsd implements [StatsD](http://github.com/etsy/statsd) metric
-tracking into php-resque.
+php-resque-influxdb implements InfluxDB metric tracking into php-resque.
 
-For each job picked up by php-resque, numerous metrics will be submitted to
-StatsD, including counters to track the number of jobs executed, and timers to
+For each job picked up by php-resque, a point will be submitted to
+InfluxDB, including tags to track the number of jobs executed, and timers to
 track how much time php-resque workers spend working.
 
-php-resque-statsd also includes support for tracking metrics for jobs scheduled
+php-resque-influxdb also includes support for tracking metrics for jobs scheduled
 with [php-resque-scheduler](http://github.com/chrisboulton/php-resque-scheduler).
 The appropriate listeners to track scheduled jobs are automatically registered,
 so no extra work is required on your behalf.
 
-## Using php-resque-statsd
+## Using php-resque-influxdb
 
-php-resque-statsd exists as a single class (`lib/ResqueStatsD.php`), which has
+php-resque-influxdb exists as a single class (`lib/InfluxDBLogger.php`), which has
 no additional dependencies beyond php-resque itself.
 
-To start tracking your jobs with StatsD, all you need to do is include
-`ResqueStatsD.php` in your project.
+To start tracking your jobs with InfluxDB, all you need to do is include
+`InfluxDBLogger.php` in your project with PSR-4. The namespace is `\Resque\Logging`.
+After this you can run `\Resque\Logging\InfluxDBLogger::register()`
 
-If you're starting php-resque with the resque.php script supplied with
-php-resque, all that is
-required is a modification to the bootstrap file you supply to php-resque via
-the `APP_INCLUDE` environment variable:
-
-	require_once '/path/to/ResqueStatsD.php';
-
-### StatsD Connection Details
+### InfluxDB Connection Details
 
 php-resque-scheduler will automatically check for the following environment
-variables if they exist and use them when connecting to StatsD:
+variables if they exist and use them when connecting to InfluxDB:
 
- * `STATSD_HOST` / `STATSD_PORT`
- * `GRAPHITE_HOST`
+ * `INFLUXDB_HOST`
+ * `INFLUXDB_PORT`
+ * `INFLUXDB_USERNAME`
+ * `INFLUXDB_PASSWORD`
 
-To ease integration with existing setups, if either `STATSD_HOST` or
-`GRAPHITE_HOST` include a single colon and then one or more numbers, this will
-be interpretted as a HOST:PORT combination and both the host and port will be
-set accordingly.
+To ease integration with existing setups, if `INFLUXDB_HOST` includes
+a single colon and then one or more numbers, this will be interpretted
+as a HOST:PORT combination and both the host and port will be set accordingly.
 
 If you don't use environment variables in your project, you can still tell
-php-resque-statsd where StatsD is located:
+php-resque-influxdb where InfluxDB is located:
 
 	$host = '127.0.0.1';
 	$port = 8579;
 
-	require_once '/path/to/ResqueStatsD.php';
-	ResqueStatsd::setHost($host, $port);
+	// Automatically register if Resque is available
+    if (class_exists('Resque') && !defined('RESQUEINFLUXDB_DONT_REGISTER'))
+    {
+        \Resque\Logging\InfluxDBLogger::register();
+    }
+	\Resque\Logging\InfluxDBLogger::setHost($host, $port);
 
 
 ## Metrics
 
-php-resque-statsd prefixes all metrics it generates with `resque`. You can
+php-resque-influxdb send all metrics it generates to the DB `resque`. You can
 override this behavior if desired:
 
-	require_once '/path/to/ResqueStatsd.php';
-	ResqueStatsd::setPrefix('resque.production');
-
-### Queue Based Metrics
-
-The metrics below are tracked for each queue, instead of each unique job:
-
-*   **stats.resque.queue.QUEUE_NAME.enqueued**
-    Counter of the number of jobs enqueued in this queue
-
-*   **stats.resque.queue.QUEUE_NAME.finished**
-	 Counter of the number of jobs successfully processed in this queue
+	\Resque\Logging\InfluxDBLogger::register();
+	\Resque\Logging\InfluxDBLogger::setDB('resque.production');
 	
-*   **stats.resque.queue.QUEUE_NAME.failed**
-	 Counter of the number of jobs that failed in this queue
+Metrics that are send have the following fields for a successful job:
 
-*   **stats.timers.queue.QUEUE_NAME.processed**
-	 Timer for jobs processed in this queue
+    'execution_time' => $executionTime,
+    'queue_time'     => $job->influxDBTimeInQueue OR 'null',
+    'start_time'     => $job->influxDBStartTime  OR 'null',
+	
+and add the following field for an unsuccessful job:
 
-*   **stats.resque.queue.QUEUE_NAME.scheduled**
-	 If using php-resque-scheduler, number of jobs scheduled for future execution
-	 in this queue
+    'error' => $e->getMessage()
 
-For example, for all jobs executed in the queue `transcode`, the following StatsD
-metrics will be created:
-
-* stats.resque.queue.transcode.enqueued
-* stats.resque.queue.transcode.finished
-* stats.resque.queue.transcode.failed
-* stats.timers.resque.queue.transcode.processed
-* stats.resque.queue.transcode.scheduled
-
-### Job Based Metrics
-
-Metrics are also tracked on a job level:
-
-*   **stats.resque.job.JOB_CLASS.enqueued**
-    Counter for the number of times this job has been enqueued
-
-*   **stats.resque.job.JOB_CLASS.finished**
-    Counter for the number of times this job has been successfully processed
-
-*   **stats.resque.job.JOB_CLASS.failed**
-    Counter for the number of times this job has failed
-
-*   **stats.timers.job.JOB_CLASS.processed**
-    Timer for the amount of time spent processing this job
-
-*   **stats.resque.job.JOB_CLASS.scheduled**
-    If using php-resque-scheduler, number of of times this job has been scheduled
-    for future execution
-
-For example, a job named `Job_SendEmail` the following metrics will be created:
-
-* stats.resque.job.Job_SendEmail.enqueued
-* stats.resque.job.Job_SendEmail.finished
-* stats.resque.job.Job_SendEmail.failed
-* stats.timers.resque.job.Job_SendEmail.processed
-* stats.resque.job.Job_SendEmail.scheduled
+And feature the job class, queue and result as tags for the metric.
 
 ## Contributors ##
 
 * chrisboulton
+* SMillerDev
---
+++ lib/InfluxDBLogger.php
@@ -0,0 +1,296 @@
+<?php
+/**
+ * php-resque-InfluxDB
+ *
+ * @package     php-resque-influxdb
+ * @author      Chris Boulton <chris@bigcommerce.com>
+ * @author      Sean Molenaar <sean@m2mobi.com>
+ * @copyright   (c) 2017 Sean Molenaar
+ * @license     http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace Resque\Logging;
+
+use InfluxDB\Client;
+use InfluxDB\Point;
+
+/**
+ * Class ResqueInfluxDB
+ */
+class InfluxDBLogger
+{
+    const INFLUXDB_TIMER   = 'ms';
+    const INFLUXDB_COUNTER = 'c';
+
+    /**
+     * Prefix to add to metrics submitted to InfluxDB.
+     * @var string
+     */
+    private static $name = 'resque';
+
+    /**
+     * Hostname when connecting to InfluxDB.
+     * @var string
+     */
+    private static $host = 'localhost';
+
+    /**
+     * Port InfluxDB is running on.
+     * @var int
+     */
+    private static $port = 8086;
+
+    /**
+     * Port InfluxDB is running on.
+     * @var int
+     */
+    private static $user = '';
+
+    /**
+     * Port InfluxDB is running on.
+     * @var int
+     */
+    private static $password = '';
+
+    /**
+     * Register php-resque-Influxdb in php-resque.
+     *
+     * Register all callbacks in php-resque for when a job is run. This is
+     * automatically called at the bottom of this script if the appropriate
+     * Resque classes are loaded.
+     *
+     * @return void
+     */
+    public static function register()
+    {
+        // Core php-resque events
+        \Resque_Event::listen('afterEnqueue', 'InfluxDBLogger::afterEnqueue');
+        \Resque_Event::listen('beforeFork', 'InfluxDBLogger::beforeFork');
+        \Resque_Event::listen('afterPerform', 'InfluxDBLogger::afterPerform');
+        \Resque_Event::listen('onFailure', 'InfluxDBLogger::onFailure');
+
+        // Add support for php-resque-scheduler
+        \Resque_Event::listen('afterSchedule', 'ResqueInfluxDB::afterSchedule');
+    }
+
+    /**
+     * Set the host/port combination of InfluxDB.
+     *
+     * @param string $host     Hostname/IP of InfluxDB server.
+     * @param int    $port     Port InfluxDB is listening on.
+     * @param string $username Username for InfluxDB
+     * @param string $password Password for InfluxDB
+     *
+     * @return void
+     */
+    public static function setHost($host, $port, $username = '', $password = '')
+    {
+        self::$host     = $host;
+        self::$port     = $port;
+        self::$user     = $username;
+        self::$password = $password;
+    }
+
+    /**
+     * Override the db for metrics that are submitted to InfluxDB.
+     *
+     * @param string $db Prefix to use for metrics.
+     *
+     * @return void
+     */
+    public static function setDB($db)
+    {
+        self::$name = $db;
+    }
+
+    /**
+     * Submit metrics for a queue and job whenever a job is pushed to a queue.
+     *
+     * @param string $class Class name of the job that was just created.
+     * @param array  $args  Arguments passed to the job.
+     * @param string $queue Name of the queue the job was created in.
+     *
+     * @return void
+     */
+    public static function afterEnqueue($class, $args, $queue)
+    {
+        //NO-OP
+    }
+
+    /**
+     * Submit metrics for a queue and job whenever a job is scheduled in php-resque-scheduler.
+     *
+     * @param \DateTime|int $at    Instance of PHP DateTime object or int of UNIX timestamp.
+     * @param string        $queue Name of the queue the job was created in.
+     * @param string        $class Class name of the job that was just created.
+     * @param array         $args  Arguments passed to the job.
+     *
+     * @return void
+     */
+    public static function afterSchedule($at, $queue, $class, $args)
+    {
+        //NO-OP
+    }
+
+    /**
+     * Begin tracking execution time before forking out to run a job in a php-resque worker
+     * and submits the metrics for the duration of a job spend waiting in the queue.
+     *
+     * Time tracking begins in `beforeFork` to ensure that the time spent for forking
+     * and any hooks registered for `beforePerform` is also tracked.
+     *
+     * @param \Resque_Job $job Instance of Resque_Job for the job about to be run.
+     *
+     * @return void
+     */
+    public static function beforeFork(\Resque_Job $job)
+    {
+        $job->influxDBStartTime = microtime(TRUE);
+
+        if (isset($job->payload['queue_time']))
+        {
+            $job->influxDBTimeInQueue = round(microtime(TRUE) - $job->payload['queue_time']) * 1000;
+        }
+    }
+
+    /**
+     * Submit metrics for a queue and job as soon as job has finished executing successfully.
+     *
+     * @param \Resque_Job $job Instance of Resque_Job for the job that's just been executed.
+     *
+     * @return void
+     */
+    public static function afterPerform(\Resque_Job $job)
+    {
+        $executionTime = round(microtime(TRUE) - $job->influxDBStartTime) * 1000;
+        self::sendMetric(
+            [
+                'execution_time' => $executionTime,
+                'queue_time'     => isset($job->influxDBTimeInQueue) ? $job->influxDBTimeInQueue : 'null',
+                'start_time'     => isset($job->influxDBStartTime) ? $job->influxDBStartTime : 'null',
+            ],
+            [
+                $job->payload['class'],
+                $job->queue,
+                'finished',
+            ]);
+    }
+
+    /**
+     * Submit metrics for a queue and job whenever a job fails to run.
+     *
+     * @param \Exception  $e   Exception thrown by the job.
+     * @param \Resque_Job $job Instance of Resque_Job for the job that failed.
+     *
+     * @return void
+     */
+    public static function onFailure(\Exception $e, \Resque_Job $job)
+    {
+        $executionTime = round(microtime(TRUE) - $job->influxDBStartTime) * 1000;
+        self::sendMetric(
+            [
+                'error'          => $e->getMessage(),
+                'execution_time' => $executionTime,
+                'queue_time'     => isset($job->influxDBTimeInQueue) ? $job->influxDBTimeInQueue : 'null',
+                'start_time'     => isset($job->influxDBStartTime) ? $job->influxDBStartTime : 'null',
+            ],
+            [
+                $job->payload['class'],
+                $job->queue,
+                'failed',
+            ]
+        );
+    }
+
+    /**
+     * Return a tuple containing the InfluxDB host and port to submit metrics to.
+     *
+     * Looks for environment variable INFLUXDB_HOST before resorting to the host/port
+     * combination passed to `register`, or defaulting to localhost.
+     * Port is determined in much the same way, however looks for the INFLUXDB_PORT environment variable.
+     * Same for INFLUXDB_PASSWORD and INFLUXDB_PASSWORD.
+     *
+     * If the host variable includes a single colon, the first part of the string
+     * is used for the host, and the second part for the port.
+     *
+     * @return Client Array containing host and port.
+     */
+    private static function getInfluxDBHost()
+    {
+        $host     = self::$host;
+        $port     = self::$port;
+        $user     = self::$user;
+        $password = self::$password;
+
+        if (!empty($_ENV['INFLUXDB_HOST']))
+        {
+            $host = $_ENV['INFLUXDB_HOST'];
+        }
+
+        if (!empty($_ENV['INFLUXDB_PORT']))
+        {
+            $port = $_ENV['INFLUXDB_PORT'];
+        }
+
+        if (!empty($_ENV['INFLUXDB_USERNAME']))
+        {
+            $user = $_ENV['INFLUXDB_USERNAME'];
+        }
+
+        if (!empty($_ENV['INFLUXDB_PASSWORD']))
+        {
+            $password = $_ENV['INFLUXDB_PASSWORD'];
+        }
+
+        if (substr_count($host, ':') == 1)
+        {
+            list($host, $port) = explode(':', $host);
+        }
+
+        $client = new Client($host, $port, $user, $password);
+
+        return $client;
+    }
+
+    /**
+     * Get a database object
+     *
+     * @return bool|\InfluxDB\Database
+     */
+    private static function getDB()
+    {
+        $client = self::getInfluxDBHost();
+
+        if (empty($client))
+        {
+            return FALSE;
+        }
+
+        $db = $client->selectDB(self::$name);
+
+        return $db;
+    }
+
+    /**
+     * Submit a metric of the given type, name and value to InfluxDB.
+     *
+     * @param array $fields Key=>Value pair to indicate fields
+     * @param array $tags   Array of tags to submit
+     *
+     * @return boolean True if the metric was submitted successfully.
+     */
+    private static function sendMetric($fields, $tags)
+    {
+        $db = self::getDB();
+
+        $point = new Point('Resque');
+        $point->setFields($fields);
+        $point->setTags($tags);
+        $point->setTimestamp(exec('date +%s%N'));
+
+        $db->writePoints([$point]);
+
+        return TRUE;
+    }
+
+}
