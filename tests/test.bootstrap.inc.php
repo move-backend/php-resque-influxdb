fe71336401916c1e6f9dd6f77f18b4015c685c34
Heinz Wiesinger <heinz@m2mobi.com>
2021-06-03 13:47:42 +0200

    Add support for decomposer

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -7,5 +7,14 @@
 
 $base = __DIR__ . '/..';
 
-// Include libraries
-require_once $base . '/vendor/autoload.php';
+if (file_exists($base . '/vendor/autoload.php') == TRUE)
+{
+    // Load composer autoloader.
+    require_once $base . '/vendor/autoload.php';
+}
+else
+{
+    // Load decomposer autoloader.
+    require_once $base . '/decomposer.autoload.inc.php';
+    autoload_register_psr4_prefix('Resque\Logging', 'lib/');
+}
